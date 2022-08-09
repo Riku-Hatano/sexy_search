3834841842e40d498598519fcd2260dd4e2d4312
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-08-09 13:50:24 +0900

    遊戯王apiに接続し、データを受け取れるようにしました。ただし、render部分でバグが起きているため動きません

--- src/JSON.js
+++ src/JSON.js
@@ -4,38 +4,49 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            loading:false,
+            loading:true,
             character:{},
         }
     }
     
-        componentDidMount(){
-        this.setState({
-            loading: true
-        })
-        fetch("https://pokeapi.co/api/v2/ability/")
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    character: data,
-                    loading: false
-                })
+    componentDidMount(){
+    this.setState({
+        loading: true
+    })
+    fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal%20Monster")
+    // fetch("https://pokeapi.co/api/v2/ability/")
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                character: data,
+                loading: false
             })
-            console.log("fetch")
-        }
+        })
+        console.log("fetch")
+    }
 
-        render() {
-        const displayText = this.state.loading ? "now loading...." : this.state.character.count
-        console.log(this.state.character.results)
+    // makeDiscriptions() {
+    //     for (let i = 0 ; i < this.state.character.data.length ; i++) {
+    //         let monster = this.state.character.data[i].name
+    //         console.log(monster)
+    //     }
+    // }
+
+
+    render() {
+        const num = this.state.loading ? "loading" : this.state.character.data.length
+        for (let i = 0 ; i < num ; i ++) {
+            const flavor = this.state.character.data[i].name
+            console.log(flavor)
+        }
         return (
             <div>
-                done
-                {displayText}
+                {flavor}
             </div>
         )
+        // const displayText = this.state.loading ? "now loading...." : this.state.character.data.length
     }
 }
-
 
 
 export default App
