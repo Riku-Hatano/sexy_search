0b83578c8f3ccf35effdfd357f6ae2157b320818
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-08-22 06:59:07 +0900

    コードをリファクタリングしました

--- src/JSON.js
+++ src/JSON.js
@@ -5,9 +5,7 @@ class App extends Component {
         super()
         this.state = {
             loading:true,
-            character1:{},
-            character2:{},
-            character3:{},
+            characters: {}
         }
     }
     
@@ -15,83 +13,96 @@ class App extends Component {
     this.setState({
         loading: true
     })
+    let characters = []
     fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Pendulum%20Normal%20Monster")
         .then(response => response.json())
         .then(data => {
+            characters.push(data)
             this.setState({
-                character1: data,
                 loading: true
             })
-            console.log(this.state.character1.data)
+            console.log(this.state.characters.data)
         })
         console.log("pendulum normal")
     fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal%20Monster")
         .then(response => response.json())
         .then(data => {
+            characters.push(data)
             this.setState({
-                character2: data,
                 loading: false
             })
-            console.log(this.state.character2.data)
+            console.log(this.state.characters.data)
         })
         console.log("normal")
     fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal%20Tuner%20Monster")
         .then(response => response.json())
         .then(data => {
+            characters.push(data)
             this.setState({
-                character3: data,
                 loading: true
             })
-            console.log(this.state.character3.data)
+            console.log(this.state.characters.data)
         })
         console.log("tuner normal")
+    this.setState({
+        characters: characters
+    })
     }
 
 
     render() {
         let div = document.getElementById("div")
-        let num = this.state.loading ? "loading" : this.state.character1.data.length
-        // let num = this.state.character1.data.length
-        console.log(num)
-        for (let i = 0 ; i < num ; i ++) {
-            const monster = this.state.character1.data[i].name
-            const flavor = this.state.character1.data[i].desc
-            const monsterItem = document.createElement("div")
-            const flavorItem = document.createElement("div")
-            monsterItem.textContent = monster
-            flavorItem.textContent = flavor
-            div.appendChild(monsterItem)
-            div.appendChild(flavorItem)
-            console.log(this.state.character1.data[i].desc)
+        for (let i = 0 ; i < this.state.characters.length ; i++) {
+            let num = this.state.loading ? "loading" : this.state.characters[i].data.length
+            for (let j = 0 ; j < num ; j ++) {
+                const monster = this.state.characters[i].data[j].name
+                const flavor = this.state.characters[i].data[j].desc
+                const monsterItem = document.createElement("div")
+                const flavorItem = document.createElement("div")
+                monsterItem.textContent = monster
+                flavorItem.textContent = flavor
+                div.appendChild(monsterItem)
+                div.appendChild(flavorItem)
+                console.log(this.state.characters[i].data[j].desc)
+            }
         }
-        div = document.getElementById("div")
-        num = this.state.loading ? "loading" : this.state.character2.data.length
-        // num = this.state.character2.data.length
-        for (let i = 0 ; i < num ; i ++) {
-            const monster = this.state.character2.data[i].name
-            const flavor = this.state.character2.data[i].desc
-            const monsterItem = document.createElement("div")
-            const flavorItem = document.createElement("div")
-            monsterItem.textContent = monster
-            flavorItem.textContent = flavor
-            div.appendChild(monsterItem)
-            div.appendChild(flavorItem)
-            console.log(this.state.character2.data[i].desc)
-        }
-        div = document.getElementById("div")
-        num = this.state.loading ? "loading" : this.state.character3.data.length
-        // num = this.state.character3.data.length
-        for (let i = 0 ; i < num ; i ++) {
-            const monster = this.state.character3.data[i].name
-            const flavor = this.state.character3.data[i].desc
-            const monsterItem = document.createElement("div")
-            const flavorItem = document.createElement("div")
-            monsterItem.textContent = monster
-            flavorItem.textContent = flavor
-            div.appendChild(monsterItem)
-            div.appendChild(flavorItem)
-            console.log(this.state.character3.data[i].desc)
-        }
+        
+        // let num = this.state.loading ? "loading" : this.state.character1.data.length
+        // for (let i = 0 ; i < num ; i ++) {
+        //     const monster = this.state.character1.data[i].name
+        //     const flavor = this.state.character1.data[i].desc
+        //     const monsterItem = document.createElement("div")
+        //     const flavorItem = document.createElement("div")
+        //     monsterItem.textContent = monster
+        //     flavorItem.textContent = flavor
+        //     div.appendChild(monsterItem)
+        //     div.appendChild(flavorItem)
+        //     console.log(this.state.character1.data[i].desc)
+        // }
+        // num = this.state.loading ? "loading" : this.state.character2.data.length
+        // for (let i = 0 ; i < num ; i ++) {
+        //     const monster = this.state.character2.data[i].name
+        //     const flavor = this.state.character2.data[i].desc
+        //     const monsterItem = document.createElement("div")
+        //     const flavorItem = document.createElement("div")
+        //     monsterItem.textContent = monster
+        //     flavorItem.textContent = flavor
+        //     div.appendChild(monsterItem)
+        //     div.appendChild(flavorItem)
+        //     console.log(this.state.character2.data[i].desc)
+        // }
+        // num = this.state.loading ? "loading" : this.state.character3.data.length
+        // for (let i = 0 ; i < num ; i ++) {
+        //     const monster = this.state.character3.data[i].name
+        //     const flavor = this.state.character3.data[i].desc
+        //     const monsterItem = document.createElement("div")
+        //     const flavorItem = document.createElement("div")
+        //     monsterItem.textContent = monster
+        //     flavorItem.textContent = flavor
+        //     div.appendChild(monsterItem)
+        //     div.appendChild(flavorItem)
+        //     console.log(this.state.character3.data[i].desc)
+        // }
 
         return (
             <div>
