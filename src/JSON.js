c5fdc786b327f17c445389ededc14fc0572bd37f
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-08-25 15:53:04 +0900

    atk,defを表示できるようにしました

--- src/JSON.js
+++ src/JSON.js
@@ -53,9 +53,17 @@ class App extends Component {
 
     render() {
         let counter = 0
-        let individualCard
+        let individualCard 
+        let monster
+        let flavor
+        let atk
+        let def
+        let monsterItem
+        let flavorItem
+        let atkItem
+        let defItem
+        let descriptions = ["name", "desc", "atk", "def"]
         const parent = document.getElementById("cardList")
-        let card = document.getElementById("card")
         for (let i = 0 ; i < this.state.characters.length ; i++) {
             let num = this.state.loading ? "loading" : this.state.characters[i].data.length
             for (let j = 0 ; j < num ; j ++) {
@@ -63,16 +71,26 @@ class App extends Component {
                 individualCard.id = counter
                 parent.appendChild(individualCard)
 
-                const monster = this.state.characters[i].data[j].name
-                const flavor = this.state.characters[i].data[j].desc
-                const monsterItem = document.createElement("div")
-                const flavorItem = document.createElement("div")
+                monster = this.state.characters[i].data[j].name
+                flavor = this.state.characters[i].data[j].desc
+                atk = this.state.characters[i].data[j].atk
+                def = this.state.characters[i].data[j].def
+                monsterItem = document.createElement("div")
+                flavorItem = document.createElement("div")
+                atkItem = document.createElement("div")
+                defItem = document.createElement("div")
                 monsterItem.className = "monsterItem parentItem"
                 flavorItem.className = "flavorItem parentItem"
+                atkItem.className = "monsterItem parentItem"
+                defItem.className = "monsterItem parentItem"
                 monsterItem.textContent = monster
                 flavorItem.textContent = flavor
+                atkItem.textContent = "atk: " + atk
+                defItem.textContent = "def: " + def
                 individualCard.appendChild(monsterItem)
                 individualCard.appendChild(flavorItem)
+                individualCard.appendChild(atkItem)
+                individualCard.appendChild(defItem)
                 // console.log(this.state.characters[i].data[j].desc)
             }
         }
