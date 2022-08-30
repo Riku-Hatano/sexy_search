0d9d7ae5bd94be6fa5403bf2e5f371f5a291b448
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-08-30 09:30:17 +0900

    domに任意の数子要素を追加できるようにしました

--- src/card.tsx
+++ src/card.tsx
@@ -1,55 +1,75 @@
+import React from "react";
 import { Component } from "react";
 
 
 
 
 const Card = (characters: any, loading: boolean, stateInfo: any) => {
-    console.log("done")
-    console.log(characters)
-    let counter = 0
-    let monster
-    let flavor
-    let atk
-    let def
-    let monsterItem
-    let flavorItem
-    let atkItem
-    let defItem
-    const parent = document.getElementById("cardList")
-    for (let i = 0 ; i < characters.length ; i++) {
-        let num = loading ? "loading" : characters[i].data.length
-        for (let j = 0 ; j < num ; j ++) {
-            const individualCard = document.createElement("div")
-            // individualCard.id = counter
-            individualCard.addEventListener("click", () => stateInfo.clicked(individualCard.id))
-            // parent.appendChild(individualCard)
-
-            monster = characters[i].data[j].name
-            flavor = characters[i].data[j].desc
-            atk = characters[i].data[j].atk
-            def = characters[i].data[j].def
-            monsterItem = document.createElement("div")
-            flavorItem = document.createElement("div")
-            atkItem = document.createElement("div")
-            defItem = document.createElement("div")
-            monsterItem.className = "monsterItem parentItem"
-            flavorItem.className = "flavorItem parentItem"
-            atkItem.className = "monsterItem parentItem"
-            defItem.className = "monsterItem parentItem"
-            monsterItem.textContent = monster
-            flavorItem.textContent = flavor
-            atkItem.textContent = "atk: " + atk
-            defItem.textContent = "def: " + def
-            individualCard.appendChild(monsterItem)
-            individualCard.appendChild(flavorItem)
-            individualCard.appendChild(atkItem)
-            individualCard.appendChild(defItem)
-            counter++
-        }
-    }
-    counter = 0       
-    
+    let box: any = []
+    const child = React.createElement("p", {key: 1, style:{color: "red"}}, "done")
+    const child2 = React.createElement("p", {key: 2}, "done2")
+    const child3 = React.createElement("p", {key: 3}, "done3")
+    box.push(child)
+    box.push(child2)
+    box.push(child3)
+    const parent2 = React.createElement("div", {}, [...box])
+    console.log(child)
+    console.log(parent2)
     // let counter = 0
+    // let monster
+    // let flavor
+    // let atk
+    // let def
+    // let monsterItem
+    // let flavorItem
+    // let atkItem
+    // let defItem
+        // for (let i = 0 ; i < characters.length ; i++) {
+        //     let num = loading ? "loading" : characters[i].data.length
+        //     for (let j = 0 ; j < num ; j ++) {
+        //         const individualCard = React.createElement("div")
+        //         // individualCard.id = counter
+        //         individualCard.addEventListener("click", () => stateInfo.clicked(individualCard.id))
+        //         // parent.appendChild(individualCard)
+
+        //         monster = characters[i].data[j].name
+        //         flavor = characters[i].data[j].desc
+        //         atk = characters[i].data[j].atk
+        //         def = characters[i].data[j].def
+        //         monsterItem = document.createElement("div")
+        //         flavorItem = document.createElement("div")
+        //         atkItem = document.createElement("div")
+        //         defItem = document.createElement("div")
+        //         monsterItem.className = "monsterItem parentItem"
+        //         flavorItem.className = "flavorItem parentItem"
+        //         atkItem.className = "monsterItem parentItem"
+        //         defItem.className = "monsterItem parentItem"
+        //         monsterItem.textContent = monster
+        //         flavorItem.textContent = flavor
+        //         atkItem.textContent = "atk: " + atk
+        //         defItem.textContent = "def: " + def
+        //         individualCard.appendChild(monsterItem)
+        //         individualCard.appendChild(flavorItem)
+        //         individualCard.appendChild(atkItem)
+        //         individualCard.appendChild(defItem)
+        //         counter++
+        //     }
+        // }
+        // counter = 0       
+
+        return (
+           <>
+           {parent2}
+           </>
+    )
+
+}
+
+
+export default Card
+
+
+// let counter = 0
     // let monster
     // let flavor
     // let atk
@@ -91,13 +111,3 @@ const Card = (characters: any, loading: boolean, stateInfo: any) => {
     //     }
     // }
     // counter = 0       
-    return (
-        <div id="cardList">
-
-        </div>
-    )
-
-}
-
-
-export default Card
