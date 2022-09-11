d5332b24706f5159237481aaeb6c841405d8fb5d
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-09-11 12:07:35 +0900

    test commit

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,23 +4,13 @@ import Monster from "./Monster"
 const Home = () => {
     return (
         <div>
-            <button onClick={() => returnMonster()}>
-                return monster
-            </button>
             <a href="/monster">monster</a>
+            <div>done</div>
             <a href="/spell">spell</a>
         </div>
     )
 }
 
-const returnMonster = () => {
-    console.log("done")
-    return (
-        <>
-        <div>done</div>
-        </>
-    )
-}
 
 
 export default Home
