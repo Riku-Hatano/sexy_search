99c8a808e9f9fa84925f73b628a7808f45e45c8c
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-09-15 17:36:21 +0900

    made new tsx files to create functions to search cards by archtypes

---
+++ src/page_tsx/Archetypes.tsx
@@ -0,0 +1,10 @@
+import react from "react"
+
+const Archetypes = () => {
+    console.log("done from archtypes")
+    return (
+        <div>done</div>
+    )
+}
+
+export default Archetypes
--- src/page_tsx/Home.tsx
+++ src/page_tsx/Home.tsx
@@ -4,9 +4,18 @@ import "../page_css/Home.css"
 const Home = () => {
     return (
         <div>
+            <head>
+                <meta charSet="utf-8"></meta>
+                <title>fetch data from Yu-Gi-Oh Api</title>
+            </head>
+            <header>
+                <h1>sth</h1>
+            </header>
             <a href="/monster">monster</a>
             <hr></hr>
             <a href="/spell">spell</a>
+            <br></br>
+            <a href="/archetypes">archtypes</a>
         </div>
     )
 }
