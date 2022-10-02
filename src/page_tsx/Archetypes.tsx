0106d0be0370610eca74ecd138dbf8336792301a
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-10-02 16:01:52 -0700

    deleted unused code and add some code

--- src/page_tsx/Archetypes.tsx
+++ src/page_tsx/Archetypes.tsx
@@ -1,4 +1,4 @@
-import react from "react"
+import react, { useEffect } from "react"
 
 import axios from "axios"
 import { useState } from "react" 
@@ -12,86 +12,49 @@ import { useParams } from "react-router-dom"
 
 
 
-
-type State = {
-    loading: boolean
-    characters: any
-    modalImage: string
-}
-
-
-
-
 const Archetypes = () => {
     const [values, setValues] = useState({
         loading: true,
         characters: {},
         modalImage: "https://storage.googleapis.com/ygoprodeck.com/pics/38955728.jpg",
     })
-    /*　
-searchCardは、個別のカードがクリックされた時、カードに応じた画像を拾って表示させる処理。
-都度http通信をする。
-*/
-    const searchCard = (cardNumber: number, id: number) => {//cardUrlは、別の方法で画像を持ってきたくなった時のために一応残してある。
-    // クリックされたカードの背景を紫色のする処理
-    let parent = document.getElementById(`individualCard${cardNumber}`) as HTMLDivElement
-    parent.classList.add("added")
-
-    // モーダルウインドウの画像のsrcを変更する処理
-    const imgsrc = `https://storage.googleapis.com/ygoprodeck.com/pics/${id}.jpg`
-    setValues({
-        ...values, 
-        modalImage: imgsrc
-    })
-    console.log(values.modalImage)
-
-    // モーダルウインドウの表示/非表示を切り替える処理
-    const parentModalWindow = document.getElementById("parentModalWindow") as HTMLDivElement
-    parentModalWindow.style.display = "inline"
-}   
-const eraseModalWindow = () => {
-    const parentModalWindow = document.getElementById("parentModalWindow") as HTMLDivElement
-    parentModalWindow.style.display = "none"
-}
-
-const ComponentDidMount = () => {
-    const { archetype } = useParams<{archetype: string}>() 
-    console.log(`done from ${archetype}`)
-    // const { archetype } = useParams();
-    console.log(useParams<{archetype: string}>())
-    console.log(archetype)
-    setValues({
-        ...values,
-        loading: true
-    })
-    
-    const characters: Array<any> = []
-    let box: any = []
-    
-    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + archetype)
-    .then((results) => {
-        const d = results.data["data"] as Array<any>
-        characters.push(d);       
-        for (let i = 0 ; i < characters.length ; i++) {
-            for (let j = 0 ; j < characters[i].length ; j++) {
-                box.push(characters[i][j])
-                console.log(characters[i][j])
-            }
-        }        
-    })
-
-    .then(() => {
+    const ComponentDidMount = () => {
+        const { archetype } = useParams<{archetype: string}>() 
         setValues({
             ...values,
-            characters: box
+            loading: true
         })
-    })
-    // catchでエラー時の挙動を定義
-    .catch(err => {
-        console.log("err:", err);
-    })
-}
-ComponentDidMount()
+        
+        const characters: Array<any> = []
+        let box: any = []
+        
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + archetype)
+        .then((results) => {
+            const d = results.data["data"] as Array<any>
+            characters.push(d);       
+            for (let i = 0 ; i < characters.length ; i++) {
+                for (let j = 0 ; j < characters[i].length ; j++) {
+                    box.push(characters[i][j])
+                    console.log(characters[i][j])
+                }
+            }        
+        })
+
+        .then(() => {
+            setValues({
+                ...values,
+                characters: box
+            })
+        })
+        // catchでエラー時の挙動を定義
+        .catch(err => {
+            console.log("err:", err);
+        })
+    }
+    ComponentDidMount();
+    // useEffect(() => {
+    //     ComponentDidMount();
+    // }, [])
     
     return (
         <div id="parentroot">
